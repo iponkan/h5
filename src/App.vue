6be2ff8bd8c5ff7d2a60bcfbe72e88b0c2657ae8
iponkan <iponkan@gmail.com>
2019-09-04 14:57:23 +0800

    小箭头动画

--- src/App.vue
+++ src/App.vue
@@ -25,12 +25,53 @@
     }
 </script>
 
-<style>
+<style lang="scss" scoped>
     html, body, #app {
         padding: 0;
         margin: 0;
         height: 100%;
         overflow: hidden;
         -webkit-overflow-scrolling: touch;
+    }
+
+    .arrow {
+        position: absolute;
+        top: 90%;
+        left: 45%;
+        width: 10%;
+        z-index: 2;
+    }
+
+    .fadeOutUp {
+        -webkit-animation: 2s fadeOutUp ease-in-out infinite;
+        animation: 2s fadeOutUp ease-in-out infinite;
+    }
+
+    @-webkit-keyframes fadeOutUp {
+        0% {
+            opacity: 1;
+            -webkit-transform: translateY(0px);
+            transform: translateY(0px);
+        }
+        50% {
+            opacity: 0.3;
+            -webkit-transform: translateY(-20px);
+            transform: translateY(-20px);
+        }
+    }
+
+    @keyframes fadeOutUp {
+        0% {
+            opacity: 1;
+            -webkit-transform: translateY(0);
+            -ms-transform: translateY(0);
+            transform: translateY(0);
+        }
+        50% {
+            opacity: 0.3;
+            -webkit-transform: translateY(-20px);
+            -ms-transform: translateY(-20px);
+            transform: translateY(-20px);
+        }
     }
 </style>
