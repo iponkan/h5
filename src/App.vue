d9434268fefd3d72fdcb4a729ca5c595b81977c5
iponkan <iponkan@gmail.com>
2019-09-04 15:20:44 +0800

    加入音乐

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,9 @@
         <Scroll style="background-color: #D81B60 ;width:100%;;height:100%">
             <CSlider style="width:100%;height:100%"/>
         </Scroll>
+        <audio id="audio"
+               src="https://m7.music.126.net/20190904154408/f442f39b5e7e408351d7a0427bd97fc6/ymusic/0152/055a/5308/681ef6f50f20971ac5bddb07161fc6b5.mp3"
+               loop="loop" autoplay="true"></audio>
     </div>
 </template>
 
