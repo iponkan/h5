201523257a55aa2aa853ace9af90d82f84865f1b
iponkan <iponkan@gmail.com>
2019-09-05 17:15:50 +0800

    视频试验

---
+++ src/components/PageVideo.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="">
+        <video class="videoClass" width="100%" height="100%" id="thevideo4" preload="auto"
+               src="https://ugcydzd.qq.com/uwMROfz2r5zBIaQXGdGnC2dfDma3J1MItM3912IN4IRQvkRM/z0829brvw04.mp4?sdtfrom=v1010&guid=5c4078c932efa138f7232597207c0cad&vkey=2B0204FD20BAC54B7E9454C5F2AB2FACFC41A24B14F35642A6DA438FFA19206121DBCCFD52A1791E5FCC270213A2E63142391CE39FEB3A2E8FE735D05D1CEC7DB9D86DB6DB5F3A1708B7C54CFD52EF69D886E207BFC7778EEF70D5B198BE682A6B4AA1FEB58F99762FC19BDB38193B4792850FF871ED08214E0FF6EFD92B6910"
+               poster="../assets/0.jpg"
+               controls="">
+        </video>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'page_video',
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+    body {
+        background-color: #000000;
+    }
+</style>
