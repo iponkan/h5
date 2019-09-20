e2a8154daefcff15529a1fc55b027a78e2650376
iponkan <iponkan@gmail.com>
2019-09-20 15:15:12 +0800

    dplayer 解决

--- src/components/Page0.vue
+++ src/components/Page0.vue
@@ -1,87 +1,32 @@
 <template>
-    <div id="app">
-        <h1>Vue-DPlayer</h1>
-        <h2>A Vue 2.x component of <a href="https://github.com/DIYgod/DPlayer" target="_blank">DPlayer</a></h2>
-        <video-player :options="options"
-                     @play="play"
-                     ref="player">
-        </video-player>
+    <div>
+        <video-player :options="videoOptions"/>
     </div>
 </template>
 
 <script>
-    import videoPlayer from "./DPlayer/DPlayer.vue";
+    import videoPlayer from "./VideoPlayer/VideoPlayer.vue";
 
     export default {
+        name: "page1",
+        components: {
+            videoPlayer
+        },
         data() {
             return {
-                options: {
-                    video: {
-                        url: 'https://v-cdn.zjol.com.cn/279761.mp4?userId=44ee8296-cff7-4580-a6de-6c4e8285995f',
-                        pic: 'https://v-img.zjol.com.cn/279757.jpg'
-                    },
+                videoOptions: {
                     autoplay: false,
-                    contextmenu: [
+                    controls: true,
+                    preload: "auto",
+                    poster: "https://v-img.zjol.com.cn/279757.jpg",
+                    sources: [
                         {
-                            text: 'GitHub',
-                            link: 'https://github.com/MoePlayer/vue-dplayer'
+                            src: "https://v-cdn.zjol.com.cn/279761.mp4?userId=44ee8296-cff7-4580-a6de-6c4e8285995f",
+                            type: "video/mp4"
                         }
                     ]
-                },
-            }
-        },
-        methods: {
-            play() {
-                const player = this.$refs.player.dp
-                player.play()
-                console.log('play callback')
-            }
-        },
-        components: {
-            videoPlayer,
+                }
+            };
         }
     }
 </script>
-
-<style>
-    body {
-        margin: 0;
-        padding: 0;
-        text-align: center;
-    }
-
-    a {
-        color: #42b983;
-        text-decoration: none;
-    }
-
-    .video_palyer {
-        width: 800px;
-        margin: 50px auto;
-    }
-
-    h1 {
-        font-size: 54px;
-        color: #42b983;
-        margin: 30px 0 10px;
-    }
-
-    h2 {
-        font-size: 22px;
-        color: #555;
-    }
-
-    @media (max-width: 768px) {
-        .video_palyer {
-            width: 90%;
-        }
-
-        h1 {
-            font-size: 30px;
-        }
-
-        h2 {
-            font-size: 16px;
-        }
-    }
-</style>
--- src/components/Page1.vue
+++ src/components/Page1.vue
@@ -1,32 +1,58 @@
 <template>
-    <div>
-        <video-player :options="videoOptions"/>
+    <div id="app">
+        <d-player @play="play" :options="options"
+                  class="page0_palyer"></d-player>
     </div>
 </template>
 
 <script>
-    import videoPlayer from "./VideoPlayer/VideoPlayer.vue";
+    import VueDPlayer from 'vue-dplayer'
+    import 'vue-dplayer/dist/vue-dplayer.css'
 
     export default {
-        name: "page1",
-        components: {
-            videoPlayer
-        },
         data() {
             return {
-                videoOptions: {
+                options: {
+                    video: {
+                        url: 'https://v-cdn.zjol.com.cn/279761.mp4?userId=44ee8296-cff7-4580-a6de-6c4e8285995f',
+                        pic: 'https://v-img.zjol.com.cn/279757.jpg'
+                    },
                     autoplay: false,
-                    controls: true,
-                    preload: "auto",
-                    poster: "https://v-img.zjol.com.cn/279757.jpg",
-                    sources: [
+                    contextmenu: [
                         {
-                            src: "https://v-cdn.zjol.com.cn/279761.mp4?userId=44ee8296-cff7-4580-a6de-6c4e8285995f",
-                            type: "video/mp4"
+                            text: 'GitHub',
+                            link: 'https://github.com/MoePlayer/vue-dplayer'
                         }
                     ]
-                }
-            };
+                },
+            }
+        },
+        methods: {
+            play() {
+                console.log('play callback')
+            }
+        },
+        components: {
+            'd-player': VueDPlayer
         }
     }
 </script>
+
+<style>
+    body {
+        margin: 0;
+        padding: 0;
+        text-align: center;
+    }
+
+    a {
+        color: #42b983;
+        text-decoration: none;
+    }
+
+    .page0_palyer {
+        width: 100%;
+        height: auto;
+    }
+</style>
+
