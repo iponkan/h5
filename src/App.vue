23c399ce118886cd3b1889df1f16227ea9ea1729
iponkan <iponkan@gmail.com>
2019-10-16 14:35:46 +0800

    合入微信分享

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
         <Scroll style="width:100%;height:100%;">
             <CSlider style="width:100%;height:100%" />
         </Scroll>
-        
+
         <audio
             id="audio"
             src="https://m7.music.126.net/20190904154408/f442f39b5e7e408351d7a0427bd97fc6/ymusic/0152/055a/5308/681ef6f50f20971ac5bddb07161fc6b5.mp3"
@@ -16,16 +16,62 @@
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
-import Scroll from "./components/Scroll.vue";
-import CSlider from "./components/CSlider.vue";
+    // import HelloWorld from './components/HelloWorld.vue'
+    import Scroll from './components/Scroll.vue'
+    import CSlider from './components/CSlider.vue'
+    import wxapi from '../src/sns/wxapi.js'
 
-export default {
-    name: "app",
-    components: {
-        Scroll,
-        CSlider
-    }
+    export default {
+        name: 'app',
+        mounted() {
+            wxapi.wxRegister(this.wxRegCallback);
+        },
+        methods: {
+            wxRegCallback() {
+                // 用于微信JS-SDK回调
+                this.wxShareTimeline()
+                this.wxShareAppMessage()
+            },
+            wxShareTimeline() {
+                // 微信自定义分享到朋友圈
+                let option = {
+                    title: '限时团购周 挑战最低价', // 分享标题, 请自行替换
+                    link: 'https://movie.douban.com/subject/25785114/', // 分享链接，根据自身项目决定是否需要split
+                    imgUrl: 'http://demo.open.weixin.qq.com/jssdk/images/p2166127561.jpg', // 分享图标, 请自行替换，需要绝对路径
+                    success: () => {
+                        alert('分享成功')
+                    },
+                    error: () => {
+                        alert('已取消分享')
+                    }
+                };
+                // 将配置注入通用方法
+                wxapi.ShareTimeline(option)
+            },
+            wxShareAppMessage() {
+                // 微信自定义分享给朋友
+                let option = {
+                    title: '限时团购周 挑战最低价', // 分享标题, 请自行替换
+                    desc: '限时团购周 挑战最低价', // 分享描述, 请自行替换
+                    link: 'https://movie.douban.com/subject/25785114/', // 分享链接，根据自身项目决定是否需要split
+                    imgUrl: 'http://demo.open.weixin.qq.com/jssdk/images/p2166127561.jpg', // 分享图标, 请自行替换，需要绝对路径
+                    success: () => {
+                        alert('分享成功')
+                    },
+                    error: () => {
+                        alert('已取消分享')
+                    }
+                };
+                // 将配置注入通用方法
+                wxapi.ShareAppMessage(option)
+            }
+        },
+        components: {
+            Scroll,
+            CSlider,
+            wxapi
+        }
+    
 };
 </script>
 
---
+++ src/sns/wxapi.js
@@ -0,0 +1,109 @@
+/**
+ * 微信js-sdk
+ * 参考文档：https://mp.weixin.qq.com/wiki?t=resource/res_main&id=mp1421141115
+ */
+import wx from 'weixin-js-sdk'
+
+const wxApi = {
+    /**
+     * [wxRegister 微信Api初始化]
+     * @param  {Function} callback [ready回调函数]
+     */
+    wxRegister(callback) {
+        wx.config({
+            debug: true,
+            appId: 'wxf8b4f85f3a794e77',
+            timestamp: 1571195817,
+            nonceStr: 'ZohqKoS8QHP1jSWb',
+            signature: '4dab03b3dcc0a275f0c46a898d6793a1e38b0833',
+            jsApiList: [
+                'checkJsApi',
+                'onMenuShareTimeline',
+                'onMenuShareAppMessage',
+                'onMenuShareQQ',
+                'onMenuShareWeibo',
+                'onMenuShareQZone',
+                'hideMenuItems',
+                'showMenuItems',
+                'hideAllNonBaseMenuItem',
+                'showAllNonBaseMenuItem',
+                'translateVoice',
+                'startRecord',
+                'stopRecord',
+                'onVoiceRecordEnd',
+                'playVoice',
+                'onVoicePlayEnd',
+                'pauseVoice',
+                'stopVoice',
+                'uploadVoice',
+                'downloadVoice',
+                'chooseImage',
+                'previewImage',
+                'uploadImage',
+                'downloadImage',
+                'getNetworkType',
+                'openLocation',
+                'getLocation',
+                'hideOptionMenu',
+                'showOptionMenu',
+                'closeWindow',
+                'scanQRCode',
+                'chooseWXPay',
+                'openProductSpecificView',
+                'addCard',
+                'chooseCard',
+                'openCard'
+            ]
+        });
+        wx.ready((res) => {
+            // 如果需要定制ready回调方法
+            if (callback) {
+                callback()
+            }
+        })
+    },
+    /**
+     * [ShareTimeline 微信分享到朋友圈]
+     * @param {[type]} option [分享信息]
+     * @param {[type]} success [成功回调]
+     * @param {[type]} error   [失败回调]
+     */
+    ShareTimeline(option) {
+        wx.onMenuShareTimeline({
+            title: option.title, // 分享标题
+            link: option.link, // 分享链接
+            imgUrl: option.imgUrl, // 分享图标
+            success() {
+                // 用户成功分享后执行的回调函数
+                option.success()
+            },
+            cancel() {
+                // 用户取消分享后执行的回调函数
+                option.error()
+            }
+        })
+    },
+    /**
+     * [ShareAppMessage 微信分享给朋友]
+     * @param {[type]} option [分享信息]
+     * @param {[type]} success [成功回调]
+     * @param {[type]} error   [失败回调]
+     */
+    ShareAppMessage(option) {
+        wx.onMenuShareAppMessage({
+            title: option.title, // 分享标题
+            desc: option.desc, // 分享描述
+            link: option.link, // 分享链接
+            imgUrl: option.imgUrl, // 分享图标
+            success() {
+                // 用户成功分享后执行的回调函数
+                option.success()
+            },
+            cancel() {
+                // 用户取消分享后执行的回调函数
+                option.error()
+            }
+        })
+    }
+};
+export default wxApi
