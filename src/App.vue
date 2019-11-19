7c611cf167f04e2d14ff8b20628f4d8cdd15751d
iponkan <iponkan@gmail.com>
2019-11-19 10:24:39 +0800

    预加载代码

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
 <template>
     <div id="app">
-        <CSlider style="width:100%;height:100%"/>
+        <section id="loading" style="width:100%;height:100%;background: black">
+            <preload-image :imgUrlArr="imgUrls" @imgAllLoaded="fn" :animationShow="false">
+                <img src="../src/assets/loading.gif"/>
+            </preload-image>
+        </section>
 
+        <section id="content" style="width:100%;height:100%">
+            <CSlider style="width:100%;height:100%"/>
+        </section>
         <audio
                 id="audio"
                 src="https://m7.music.126.net/20190904154408/f442f39b5e7e408351d7a0427bd97fc6/ymusic/0152/055a/5308/681ef6f50f20971ac5bddb07161fc6b5.mp3"
@@ -14,6 +21,7 @@
 <script>
     import CSlider from './components/CSlider.vue'
     import wxapi from '../src/sns/wxapi.js'
+    import preloadImage from './components/util/vue-preload-image.vue'
 
     const shareData = {
         url: "https://uss.sonicers.com/guimanlong/index.html",
@@ -34,6 +42,40 @@
                 new this.$vconsole();
             }
             wxapi.wxRegister(this.wxRegCallback);
+        },
+        data() {
+            return {
+                imgUrls: ['../src/assets/bg.jpeg',
+                    '../src/assets/bg.jpeg',
+                    '../src/assets/img1.png',
+                    '../src/assets/img2.png',
+                    '../src/assets/img3.png',
+                    '../src/assets/img4.png',
+                    '../src/assets/img5.png',
+                    '../src/assets/img6.png',
+                    '../src/assets/img7.png',
+                    '../src/assets/img8.png',
+                    '../src/assets/img9.jpg',
+                    '../src/assets/img10.gif',
+                    '../src/assets/img11.jpg',
+                    '../src/assets/img12.jpg',
+                    '../src/assets/img13.jpg',
+                    '../src/assets/img14.jpg',
+                    '../src/assets/img15.jpg',
+                    '../src/assets/img16.jpg',
+                    '../src/assets/img17.png',
+                    '../src/assets/img18.png',
+                    '../src/assets/img19.jpg',
+                    '../src/assets/img20.jpg',
+                    '../src/assets/img21.jpg',
+                    '../src/assets/img22.jpg',
+                    '../src/assets/img24.jpg',
+                    '../src/assets/img25.png',
+                    '../src/assets/img26.png',
+                    '../src/assets/img27.jpg',
+                    '../src/assets/img28.png'],
+                preLoaded: false // 预加载是否完成
+            }
         },
         methods: {
             wxRegCallback() {
@@ -71,11 +113,16 @@
                     }
                 };
                 wxapi.ShareAppMessage(option)
+            },
+            fn() {
+                this.preLoaded = true;
+                document.getElementById('loading').remove()
             }
         },
         components: {
             CSlider,
-            wxapi
+            wxapi,
+            preloadImage
         }
 
     };
